232fc16cd37574be865165fac16b84759dadb1a9
marwi509 <marwi509@student.liu.se>
2016-10-09 09:25:48 +0200

    added presentation layer

---
+++ src/main/java/com/tagcloud/TagCloudFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.tagcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagCloudFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagCloudFrontendApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/js/tagcloud.js
@@ -0,0 +1,50 @@
+angular.module('tagcloud', [])
+    .controller('tagcloudController', function($scope, $http) {
+
+        $scope.host = 'http://localhost:8080';
+        $scope.hideError = true;
+
+        $scope.getUsingHashTag = function() {
+            $http.get($scope.host + '/tag-cloud-twitter?source='+encodeURIComponent($scope.tag)).then(
+                function(response) {
+                    createCloud(response.data);
+            }, function() {
+                    showError();
+            })};
+
+        $scope.getUsingRss = function() {
+            $http.get($scope.host + '/tag-cloud-rss?source='+encodeURIComponent($scope.rss)).then(
+                function(response) {
+                    createCloud(response.data);
+            }, function() {
+                showError();
+            })};
+
+        function showError() {
+            $scope.hideError = false;
+            setTimeout(function() {
+                $scope.hideError = true;
+            }, 3000);
+        }
+
+        function createCloud(words) {
+            clearCloud();
+            d3.wordcloud()
+                .size([800, 400])
+                .selector('#cloud')
+                .words(
+                    words.map(function (item) {
+                        var newObj = {};
+                        newObj.text = item.word;
+                        newObj.size = item.weight;
+                        return newObj;
+                    }))
+                .start();
+        }
+
+        function clearCloud() {
+            var myEl = angular.element(document.querySelector('#cloud'));
+            myEl.empty();
+        }
+
+    });
